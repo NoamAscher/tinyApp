2f78b9cac516282e0b24f4112776fa7c75282fd1
Noam Ascher <noam.ascher@gmail.com>
2016-10-17 21:45:56 +0000

    links only can be edited / deleted by logged in users.

--- express_server.js
+++ express_server.js
@@ -23,13 +23,13 @@ function generateRandomString() {
 
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  "b2xVn2": {link: "http://www.lighthouselabs.ca", id: ""},
+  "9sm5xK": {link: "http://www.google.com", id: ""}
 };
 
 // added during User Registration portion of project:
 var users = {
-  "guest": {id: "guest", email: "", password: ""}
+ // "guest": {id: "guest", email: "", password: ""}
 };
 
 
@@ -44,7 +44,7 @@ var templateVars = {
 
 
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.end("<html><body><h4>Hello!</h4><a href=\"/urls\">Go to list of URLs</a><body><html>");
 });
 
 app.listen(PORT, () => {
@@ -64,8 +64,12 @@ app.get("/urls", (req, res) => {
   if (req.cookies.userId){
     templateVars.userId = req.cookies.userId;
     templateVars.email = users[req.cookies.userId].email;
-    res.render("urls_index", templateVars);
+  } else {
+    // *** ... is this a good idea? ***
+    templateVars.userId = "";
+    templateVars.email = "";
   }
+  console.log(users);
   res.render("urls_index", templateVars);
 });
 
@@ -74,6 +78,7 @@ app.get("/urls/new", (req, res) => {
   //   userId: req.cookies.userId,
   //   email: users.userId.email
   // };
+  console.log(users);
   res.render("urls_new", templateVars);
 });
 
@@ -87,6 +92,7 @@ app.get("/urls/:id", (req, res) => {
   //   shortURL: req.params.id,
   //   longURL: urlDatabase[req.params.id]
   // };
+  console.log(users);
   res.render("urls_show", templateVars);
 });
 
@@ -105,7 +111,7 @@ app.post("/urls/create", (req, res) => {
   if (!(theUrl.slice(0,6) == "http://" || theUrl.slice(0,7) == "https://")) {
     theUrl = `http://${theUrl}`;
   }
-  urlDatabase[generateRandomString()] = theUrl;
+  urlDatabase[generateRandomString()] = {link: theUrl, id: req.cookies.userId};
   res.redirect("/urls");
 });
 
@@ -160,7 +166,7 @@ app.post("/urls/:id/update", (req, res) => {
 app.post("/logout", (req, res) => {
   res.clearCookie("userId");
   userId = "guest";
-  res.redirect('/login');
+  res.redirect('/urls');
 })
 
 // -------- User Reg functions --------
@@ -176,13 +182,21 @@ app.post("/register", (req, res) => {
   let newUserID = generateRandomString();
   var entryIssue = false;
   if (!req.body.email || !req.body.password) {
-    res.status(400).send('400! Fill in all the blanks please :)');
+    // ugly ugly ugly I know.
+    let line1 = "<p>400! Fill in all the blanks please :)</p>";
+    let line2 = "<div><a href=\"/register\">Retry</a></div></body></html>";
+    let line3 = "<div><a href=\"/login\">Login</a></div>";
+    res.status(400).send(`<html><body>${line1}${line2}${line3}</body></html>`);
     entryIssue = true;
     console.log("entryIssue = true");
   } else {
     for (entry in users) {
       if (req.body.email === users[entry].email) {
-        res.status(400).send('400! This email address is already registered.');
+        // ugly ugly ugly I know.
+        let line1 = "<p>400! This email address is already registered.</p>";
+        let line2 = "<div><a href=\"/register\">Retry</a></div></body></html>";
+        let line3 = "<div><a href=\"/login\">Login</a></div>";
+        res.status(400).send(`<html><body>${line1}${line2}${line3}</body></html>`);
         entryIssue = true;
         console.log("entryIssue = true");
         break;
@@ -196,8 +210,9 @@ app.post("/register", (req, res) => {
       password: req.body.password
     };
     res.cookie("userId", newUserID);
-    console.log(users);
-    // res.redirect('/');   <----- why? coded next line instead.
+    console.log('** registration success **')
+    //console.log(users); <- works, ie users successfully loaded
+    // res.redirect('/');   Coded next line instead, '/' used for a (lo-fi) splash page.
     res.redirect('/urls');
   }
 })
@@ -212,7 +227,11 @@ app.post("/login", (req, res) => {
     res.redirect('/register');
   } else {
     if (!req.body.email || !req.body.password) {
-      res.status(400).send('400! Fill in all the blanks please :)');
+      // ugly ugly ugly I know.
+      let line1 = "<p>400! Fill in all the blanks please :)</p>";
+      let line2 = "<div><a href=\"/login\">Retry</a></div></body></html>";
+      let line3 = "<div><a href=\"/register\">Register</a></div>";
+      res.status(400).send(`<html><body>${line1}${line2}${line3}</body></html>`);
       entryIssue = true;
       console.log("entryIssue = true");
     } else {
@@ -230,7 +249,11 @@ app.post("/login", (req, res) => {
         }
       }
       if (!allOk) {
-        res.status(400).send('Email not found.');
+        // ugly ugly ugly I know.
+        let line1 = "<p>400! Email not found.</p>";
+        let line2 = "<div><a href=\"/login\">Retry</a></div></body></html>";
+        let line3 = "<div><a href=\"/register\">Register</a></div>";
+        res.status(400).send(`<html><body>${line1}${line2}${line3}</body></html>`);
       }
     }
   }
